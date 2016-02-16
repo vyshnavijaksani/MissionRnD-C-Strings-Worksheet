c4353f720adc680bfebd82b1dc9decb24e834a5b
Abhijith Ravuri <KH9126@kitspl.com>
2016-02-16 17:18:46 +0530

    HeadersAdded

---
+++ src/MainStrings1.cpp
@@ -0,0 +1,32 @@
+/*
+
+Use MainStrings1 File for testing any other function in other Cpp files of the Strings Project.
+
+DO NOT write main function in any other File or else your Build Fails.
+
+Objectives of Strings-1 Lesson:
+
+->Basics of Strings / Char Arrays .
+->Learn about 2D Strings
+->Convert Numbers/Floats into Strings
+->Manipulating Strings
+
+*/
+#include <stdio.h>
+#include "FunctionHeadersStrings1.h"
+
+int main(){
+
+	//Test KthIndexFromEnd 
+
+	//TestNumbertoStr
+
+	//removeSpaces
+
+	//StrWordsinRev
+
+	//commonWords
+
+
+	return 0;
+}
